78675733430028164a0b9778eca4b37ca6716038
MoonSangWon <kuim75@naver.com>
2022-09-08 14:21:27 +0900

    test jenkins with spring framework

--- src/main/java/io/getarrays/userservice/repo/RoleRepo.java
+++ src/main/java/io/getarrays/userservice/repo/RoleRepo.java
@@ -4,6 +4,7 @@ import io.getarrays.userservice.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
+    
     Role findByName(String name);
 
 }
